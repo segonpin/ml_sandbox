adea16fe41b512ea6ca9a6274f75dee36118fa24
sebas <segonpin@gmail.com>
2025-01-28 18:50:09 +0100

    Add dataset_diabetes to get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -3,6 +3,10 @@ set -euxo pipefail
 
 DATA_DIR=data
 mkdir -p $DATA_DIR
-wget "http://s3.amazonaws.com/h2o-public-test-data/smalldata/prostate/prostate.csv.zip" -O $DATA_DIR/prostate.csv.zip
-unzip $DATA_DIR/prostate.csv.zip -d $DATA_DIR
+
+# wget "http://s3.amazonaws.com/h2o-public-test-data/smalldata/prostate/prostate.csv.zip" -O $DATA_DIR/prostate.csv.zip
+# unzip $DATA_DIR/prostate.csv.zip -d $DATA_DIR
+
+wget "https://h2o-public-test-data.s3.amazonaws.com/smalldata/diabetes/dataset_diabetes.zip" -O $DATA_DIR/dataset_diabetes.zip
+unzip $DATA_DIR/dataset_diabetes.zip -d $DATA_DIR
 
