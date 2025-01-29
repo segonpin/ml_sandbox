2b604cdb3ea5af455f353c95f82f6c29d7707a9d
sebas <segonpin@gmail.com>
2025-01-29 16:10:09 +0100

    Add zillow data

--- get_data.sh
+++ get_data.sh
@@ -7,6 +7,9 @@ mkdir -p $DATA_DIR
 # wget "http://s3.amazonaws.com/h2o-public-test-data/smalldata/prostate/prostate.csv.zip" -O $DATA_DIR/prostate.csv.zip
 # unzip $DATA_DIR/prostate.csv.zip -d $DATA_DIR
 
-wget "https://h2o-public-test-data.s3.amazonaws.com/smalldata/diabetes/dataset_diabetes.zip" -O $DATA_DIR/dataset_diabetes.zip
-unzip $DATA_DIR/dataset_diabetes.zip -d $DATA_DIR
+# wget "https://h2o-public-test-data.s3.amazonaws.com/smalldata/diabetes/dataset_diabetes.zip" -O $DATA_DIR/dataset_diabetes.zip
+# unzip $DATA_DIR/dataset_diabetes.zip -d $DATA_DIR
+
+wget "http://s3.amazonaws.com/h2o-public-test-data/bigdata/server/timeseries/zillow-housing-prices-test.csv" -O $DATA_DIR/zillow-test.csv
+wget "http://s3.amazonaws.com/h2o-public-test-data/bigdata/server/timeseries/zillow-housing-prices-train.csv" -O $DATA_DIR/zillow-train.csv
 
